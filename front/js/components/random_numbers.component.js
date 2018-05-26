b47a58e43152739a4ace6a161478941dd030efd6
MiroslawDubaj <miroslaw.dubaj@gmail.com>
2018-05-26 02:34:30 +0200

    Task done

--- front/js/components/random_numbers.component.js
+++ front/js/components/random_numbers.component.js
@@ -1,38 +1,21 @@
-function RandomNumbers(selector) {
-  Component.call(this, selector);
-  this.randomNumbers = [];
-};
+class RandomNumbers extends Ranking {
+  constructor (selector) {
+    super(selector);
+    this.randomNumbers = [];
+  }
 
-RandomNumbers.prototype = Object.create(Component.prototype);
-RandomNumbers.constructor = RandomNumbers;
-
-RandomNumbers.prototype.init = function () {
-  const self = this;
-  return axios.get('http://localhost:3000/random-numbers')
-    .then(response => {
-      self.randomNumbers = response.data.data.map(function (number) {
-        return {
-          id: number
-        };
+  init() {
+    return axios.get('http://localhost:3000/random-numbers')
+      .then(response => {
+        this.randomNumbers = response.data.data.map(number => {
+          return {
+            id: number
+          };
+        });
+      })
+      .then(() => this.render(this.randomNumbers))
+      .catch(error => {
+        console.error(error);
       });
-    })
-    .then(() => self.render())
-    .catch(error => console.error(error)
-  );
-};
-
-RandomNumbers.prototype.render = function () {
-  const container = this.getDOMElement();
-  if (container.hasChildNodes()) {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    };
   };
-  
-  this.randomNumbers.forEach(number => {
-    const listElement = document.createElement('li');
-    listElement.classList.add('list-group-item');
-    listElement.innerHTML = number.id;
-    container.appendChild(listElement);
-  });  
 };
